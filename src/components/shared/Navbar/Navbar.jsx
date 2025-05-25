80eebea8088fde3eef9aa999ff587efce6436e48
Rishad1404 <rishadislam2019@gmail.com>
2025-05-25 23:16:56 +0600

    navbar

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
 
 
 const Navbar = () => {
+    const navLinks=<>
+        <NavLink to='/'><li className='mr-3'>Home</li></NavLink>
+        <NavLink to='/'><li className='mr-3'>About</li></NavLink>
+        <NavLink to='/'><li className='mr-3'>Contact</li></NavLink>
+        <NavLink to='/'><li className='mr-3'>Properties</li></NavLink>
+    </>
     return (
         <div className="navbar shadow-md bg-base-100">
             <div className="navbar-start">
@@ -13,36 +19,19 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navLinks}
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navLinks}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn mr-3">Login</a>
+                <a className="btn">SignUp</a>
             </div>
         </div>
     );
